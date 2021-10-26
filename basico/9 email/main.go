7e6f3ad571c3ef483141b6042e1ae21f6e53c55d
MarceloLuisDantas <marceloluis257gh@gmail.com>
2021-10-25 23:56:26 -0300

    Envio de Email

--- basico/9 email/main.go
+++ basico/9 email/main.go
@@ -1,35 +1,40 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net/smtp"
+	"os"
 )
-
-var user = []string{
-	"dromedariodechapeu@gmail.com",
-	"1254marcelo",
-}
 
 var sd = []string{
 	"smtp.gmail.com",
 	"587",
 }
 
-func SendEmail(msg []byte) string {
-	to := []string{
-		"marceloluis257@gmail.com",
-	}
+func SendEmail(msg []byte, user []string, to []string) string {
 	// Authentication.
 	auth := smtp.PlainAuth("", user[0], user[1], sd[0])
 	// Sending email.
-	err := smtp.SendMail(sd[0]+":"+sd[1], auth, user[9], to, msg)
+	err := smtp.SendMail(sd[0]+":"+sd[1], auth, user[0], to, msg)
 	if err != nil {
 		log.Fatal("Erro, ", err)
 	}
 	return "Email enviado com sucesso, eu acho"
 }
 
+func input(label string, s *bufio.Scanner) string {
+	fmt.Printf("%s", label)
+	s.Scan()
+	return s.Text()
+}
+
 func main() {
-	msg := []byte("Gostoso")
-	SendEmail(msg)
+	scanner := bufio.NewScanner(os.Stdin)
+	email := input("E-Mail: ", scanner)
+	pass := input("Senha: ", scanner)
+	envio := input("Destino: ", scanner)
+	msg := input("Mensagem: ", scanner)
+	SendEmail([]byte(msg), []string{email, pass}, []string{envio})
 }
